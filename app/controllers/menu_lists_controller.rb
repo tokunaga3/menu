06e842397a6e054fc8befed770a8fa3f3de9e511
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2021-03-11 09:52:43 +0000

    binding.pryができるようにした

--- app/controllers/menu_lists_controller.rb
+++ app/controllers/menu_lists_controller.rb
@@ -1,11 +1,10 @@
 class MenuListsController < ApplicationController
   def index
-    @q = Event.ransack(params[:q])
-    @events = @q.result(distinct: true).page(params[:page]).per(PER)
+    @menu_list = MenuList.all
   end
 
   def new
-    @event = Event.new
+    @menu_list = MenuList.new
   end
 
   def show
@@ -15,45 +14,26 @@ class MenuListsController < ApplicationController
   end
 
   def update
-    if params[:participat_status] == "申し込み"
-      @event.update(participat_id: current_user.id)
-      redirect_to events_path, notice: "参加を申し込みました！"
-    elsif params[:participat_status] == "cancel"
-      @event.update(participat_id: nil)
-      redirect_to users_path, notice: "参加をキャンセルしました！"
-    elsif @event.update(event_params) && params[:participat_status] != "申し込み" && params[:participat_status] != "cancel"
-      redirect_to events_path
-    else
-      render :edit
-    end
   end
 
   def destroy
-    if params[:participat_status] == "done"
-      @event.destroy
-      redirect_to users_path, notice:"イベントを削除しました！"
-    else
-      @event.destroy
-      redirect_to events_path, notice:"イベントを削除しました！"
-    end
   end
 
   def create
-    @event = Event.new(event_params)
-    @event.user_id = current_user.id
-    if @event.save
-      redirect_to events_path,notice: "イベントを追加しました！"
+    @menu_list = MenuList.new(menu_list_params)
+    if @menu_list.save
+      redirect_to menu_lists_path,notice: "メニューを追加しました！"
     else
       render :new
     end
   end
 
   private
-    def event_params
-      params.require(:event).permit( :date, :title, :content, :start_point, :goal_point, :carry_price, :status, :participation, :participat_id)
+    def menu_list_params
+      params.require(:menu_list).permit( :menu_name, :user_id, :family_id)
     end
 
-    def set_event
-      @event = Event.find(params[:id])
+    def set_menu_list
+      @menu_list = MenuList.find(params[:id])
     end
 end
