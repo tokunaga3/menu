6ef32058bbd52034cc19dbd3afe912cefd12f89e
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2021-03-13 09:43:20 +0000

    deviceのルーティングを編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+ # このアクションを追加
+ def after_sign_in_path_for(resource)
+   menu_lists_path
+ end
+
+ def after_sign_out_path_for(resource)
+   new_user_session_path # ログアウト後に遷移するpathを設定
+ end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
