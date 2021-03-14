0a2e7d2e8f8c189f40dbf0909498ea8114a73c20
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2021-03-14 09:13:02 +0000

    ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :menu_lists, dependent: :destroy
   has_many :families, dependent: :destroy
 end
