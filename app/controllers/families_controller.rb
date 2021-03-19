786b7f7a5c2c060d36e02065d98f758cceab7e33
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2021-03-19 12:14:47 +0000

    ajax実装

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -10,11 +10,23 @@ class FamiliesController < ApplicationController
     @family = Family.new(family_params)
     @family.user_id = current_user.id
     if @family.save
-      redirect_to menu_lists_path,notice: "家族の名前を追加しました"
+      format.js { render :index,notice: "家族の名前を追加しました" }
+      # redirect_to menu_lists_path,notice: "家族の名前を追加しました"
     else
-      render :new
+      format.html { redirect_to menu_lists_path,notice: "家族の名前を追加しました" }
     end
   end
+
+  #ajax実装用の保険
+  # def create
+  #   @family = Family.new(family_params)
+  #   @family.user_id = current_user.id
+  #   if @family.save
+  #     redirect_to menu_lists_path,notice: "家族の名前を追加しました"
+  #   else
+  #     render :new
+  #   end
+  # end
 
   def family_params
     params.require(:family).permit( :family_name)
--- app/controllers/menu_lists_controller.rb
+++ app/controllers/menu_lists_controller.rb
@@ -1,16 +1,16 @@
 class MenuListsController < ApplicationController
   before_action :set_menu_list, only: [:edit, :update, :destroy]
-  before_action :set_family, only: [:show, :new]
+  before_action :set_family, only: [:show, :index]
   before_action :user_signed_in?
   before_action :authenticate_user!
 
   def index
     @menu_list = current_user.menu_lists
+    @menu_lists = MenuList.new
   end
 
-  def new
-    @menu_list = MenuList.new
-  end
+  # def new
+  # end
 
   def show
     @menu_list = current_user.menu_lists
@@ -24,15 +24,17 @@ class MenuListsController < ApplicationController
 
   def destroy
   end
-
   def create
+    @menu_lists = current_user.menu_lists
     @menu_list = MenuList.new(menu_list_params)
     @menu_list.user_id = current_user.id
     @menu_list.family_id = Family.find_by(family_name:params[:menu_list][:family_id]).id
-    if @menu_list.save
-      redirect_to menu_lists_path,notice: "メニューを追加しました！"
-    else
-      render :new
+    respond_to do |format|
+      if @menu_list.save
+        format.js { render :index }
+      else
+        format.html { redirect_to menu_list_path(@menu_list), notice: '投稿できませんでした...' }
+      end
     end
   end
 
@@ -42,7 +44,6 @@ class MenuListsController < ApplicationController
     end
 
     def set_menu_list
-      binding.pry
       @menu_list = MenuList.find(params[:id])
     end
 
@@ -50,3 +51,58 @@ class MenuListsController < ApplicationController
       @family = Family.where(user_id:current_user.id)
     end
 end
+
+
+# 保険用
+# class MenuListsController < ApplicationController
+#   before_action :set_menu_list, only: [:edit, :update, :destroy]
+#   before_action :set_family, only: [:show, :new]
+#   before_action :user_signed_in?
+#   before_action :authenticate_user!
+#
+#   def index
+#     @menu_list = current_user.menu_lists
+#   end
+#
+#   def new
+#     @menu_list = MenuList.new
+#   end
+#
+#   def show
+#     @menu_list = current_user.menu_lists
+#   end
+#
+#   def edit
+#   end
+#
+#   def update
+#   end
+#
+#   def destroy
+#   end
+#
+#   def create
+#     @menu_list = MenuList.new(menu_list_params)
+#     @menu_list.user_id = current_user.id
+#     @menu_list.family_id = Family.find_by(family_name:params[:menu_list][:family_id]).id
+#     if @menu_list.save
+#       redirect_to menu_lists_path,notice: "メニューを追加しました！"
+#     else
+#       render :new
+#     end
+#   end
+#
+#     private
+#     def menu_list_params
+#       params.require(:menu_list).permit( :menu_name, :user_id, :image )
+#     end
+#
+#     def set_menu_list
+#       binding.pry
+#       @menu_list = MenuList.find(params[:id])
+#     end
+#
+#     def set_family
+#       @family = Family.where(user_id:current_user.id)
+#     end
+# end
