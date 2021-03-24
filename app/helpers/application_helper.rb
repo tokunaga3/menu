b1b14373aa7ae62f38ceaca25b1575fa1ba3908c
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2021-03-24 13:19:34 +0000

    モーダルのヘッダーデザイン修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
   def have_image?(menu_list)
-    menu_list.image.url =="default.jpg"
+    unless menu_list.image.url =="default.jpg"
+       image_tag(menu_list.image.url)
+    end
+  end
+
+  def this_menu(families,menu_list)
+    families.find(menu_list.family_id)
   end
 end
