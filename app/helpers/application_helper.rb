435a8e51bbbceadd09ab6f05ada3558711e779ef
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2021-03-25 04:43:37 +0000

    編集のlink_toにmodalを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,15 @@
 module ApplicationHelper
   def have_image?(menu_list)
     unless menu_list.image.url =="default.jpg"
-       image_tag(menu_list.image.url)
+       image_tag menu_list.image.url, class:"img-fluid" ,alt:"Responsive image"
     end
   end
 
   def this_menu(families,menu_list)
     families.find(menu_list.family_id)
+  end
+
+  def modal_data_target(menu_list_id)
+    "#modal-e#{menu_list_id}"
   end
 end
