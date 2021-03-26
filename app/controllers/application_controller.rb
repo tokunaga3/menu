db4e896e3ca60efc1c702f38b45c12173acbb86f
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2021-03-26 13:48:55 +0000

    deviceのconfigを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
- # このアクションを追加
  def after_sign_in_path_for(resource)
    menu_lists_path
  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  devise_scope :user do
-    get "user/:id", :to => "users/registrations#detail"
-    get "signup", :to => "users/registrations#new"
-    get "login", :to => "users/sessions#new"
-    get "logout", :to => "users/sessions#destroy"
-  end
+  # devise_scope :user do
+    # get "user/:id", :to => "users/registrations#detail"
+    # get "signup", :to => "users/registrations#new"
+    # get "login", :to => "users/sessions#new"
+  #   get "logout", :to => "users/sessions#destroy"
+  # end
 
   root "menu_lists#index"
   resources :menu_lists do
