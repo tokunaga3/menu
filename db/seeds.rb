5ae6e2fa99ff185c9ad027554705333636eb700b
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2021-04-01 09:14:49 +0000

    seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+  user_name = User.where(name:"Guest")
+  user_id = user_name.ids[0]
+
+  @set_guest_family = ["息子","娘","母","父"]
+
+  @set_guest_family.each do |guest_family|
+    Family.create!(
+      family_name: guest_family,
+      user_id: user_id,
+    )
+  end
+
+  @guest_menu_list = ["オムライス","チキン南蛮","カレー","シチュー","ハヤシライス","親子丼","カルボナーラ","唐揚げ","すき焼き","お好み焼き"]
+  @guest_families = Family.where(user_id:user_id)
+  @randam_guest_family = @guest_families.offset(rand(3)).first
+
+
+  @guest_menu_list.each do |guest_menu_list|
+    MenuList.create!(
+      menu_name: guest_menu_list,
+      user_id: user_id,
+      family_id: @randam_guest_family.id,
+                 )
+  end
+# end
