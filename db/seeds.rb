492a69834a761b080d89b97a9af761ddbfb4c564
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2021-04-02 00:53:20 +0000

    seedで画像の投入成功

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,15 +20,18 @@
     )
   end
 
-  @guest_menu_list = ["オムライス","チキン南蛮","カレー","シチュー","ハヤシライス","親子丼","カルボナーラ","唐揚げ","すき焼き","お好み焼き"]
+  # @guest_menu_list = ["オムライス","チキン南蛮","カレー","シチュー","ハヤシライス","親子丼","カルボナーラ","唐揚げ","すき焼き","お好み焼き"]
+  @guest_menu_list = { "オムライス":"./public/imaage/omurasu.png","チキン南蛮":"./public/imaage/food_chicken_nanban.png","カレー":"./public/imaage/curry.png","シチュー":"./public/imaage/stew.png","ハヤシライス":"./public/imaage/hayashi_rice.png","親子丼":"./public/imaage/food_oyakodon.png","カルボナーラ":"./public/imaage/carbonara.png","唐揚げ":"./public/imaage/food_karaage_cup.png","すき焼き":"./public/imaage/food_sukiyaki.png","お好み焼き":"./public/imaage/okonomiyaki.png" }
+  # { first_name: "Yusuke", last_name: "Higaki" }
   @guest_families = Family.where(user_id:user_id)
 
 
-  @guest_menu_list.each do |guest_menu_list|
+  @guest_menu_list.each do |guest_menu_list,image|
     @randam_guest_family = @guest_families.offset(rand(3)).first
     MenuList.create!(
       menu_name: guest_menu_list,
       user_id: user_id,
       family_id: @randam_guest_family.id,
+      image:File.open(image),
                  )
   end
