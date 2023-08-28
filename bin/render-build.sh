112a57f97e158965f041aa2ba02b1afd8646097c
tokunaga3 <mutsukitokunaga@outlook.jp>
2023-08-28 22:21:57 +0900

    deploy用に修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-bundle lock --add-platform ruby
-bundle lock --add-platform x86_64-linux
+rm Gemfile.lock
+gem install bundler -v 1.17.3
+bundler _1.17.3_ install
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
