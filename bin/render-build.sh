b7f06937524d5d6779ddb6a71caf74b9b39f6d10
tokunaga3 <mutsukitokunaga@outlook.jp>
2023-08-29 19:43:55 +0900

    deploy用に修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-bundle lock --add-platform ruby
-bundle lock --add-platform x86_64-linux
-gem install bundler -v 1.17.3
-bundle lock --add-platform x86_64-linux
+
+bundle update
 
 bundle install
 bundle lock --add-platform x86_64-linux
