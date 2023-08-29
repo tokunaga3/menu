d92f76eec555ebf186aa816888a00efccb873273
tokunaga3 <mutsukitokunaga@outlook.jp>
2023-08-29 21:16:48 +0900

    deploy用に修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,11 +2,11 @@
 # exit on error
 set -o errexit
 
-bundle update
+# bundle update
 
-bundle install
+# bundle install
 bundle lock --add-platform x86_64-linux
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
-bundle exec rake db:seed
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
+# bundle exec rake db:migrate
+# bundle exec rake db:seed
