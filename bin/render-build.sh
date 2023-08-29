b01adc4f1f51a59c68e47e9a828eeb49a8e83d77
tokunaga3 <mutsukitokunaga@outlook.jp>
2023-08-29 21:24:15 +0900

    deploy用に修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,9 +3,11 @@
 set -o errexit
 
 # bundle update
-
+rm Gemfile.lock
+gem install bundler -v 1.17.3
+bundler _1.17.3_ install
 # bundle install
-bundle lock --add-platform x86_64-linux
+# bundle lock --add-platform x86_64-linux
 # bundle exec rake assets:precompile
 # bundle exec rake assets:clean
 # bundle exec rake db:migrate
