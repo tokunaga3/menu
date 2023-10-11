7792b67408aba6d0c3332928d729a0013d2d1951
tokunaga3 <mutsukitokunaga@outlook.jp>
2023-10-11 17:32:14 +0900

    dockerfileの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-gem install nokogiri --platform=ruby
+
+bundle config set force_ruby_platform true
 bundle exec rake  db:migrate
 bundle exec rake db:seed
 bundle exec puma -C config/puma.rb
