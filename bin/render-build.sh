f4e9fcf699e6dccbfe875fd55707270be832586d
tokunaga3 <mutsukitokunaga@outlook.jp>
2023-10-11 17:46:16 +0900

    dockerfileの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,7 @@
 # exit on error
 set -o errexit
 gem update --system
+bundle config set force_ruby_platform true
 bundle exec rake  db:migrate
 bundle exec rake db:seed
 bundle exec puma -C config/puma.rb
